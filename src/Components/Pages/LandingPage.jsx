079d3bbf221df506715c0f689b0e95cf61cfbb1d
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-12-01 12:34:31 -0500

    LandingPage

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -3,11 +3,28 @@ E-mail: sadw621@gmail.com */
 
 import React from 'react';
 
+import AuxNavbar from '../Modules/AuxNavbar';
+import MainFooter from '../Modules/MainFooter';
+
 function LandingPage() {
 
   return (
 
-    <div></div>
+    <>
+
+      <AuxNavbar />
+
+      <div className="main-container">
+
+        <div className="c-container">
+          <h1>Bienvenido/a a Los Proyectos</h1>
+          <h3>Si todavía no haces parte de nuestro sistema, regístrate.</h3>
+          <p>Los Proyectos es un portal que te permite administrar los proyectos de tu constructora de manera ágil y segura.</p>
+        </div>
+
+      </div>
+
+    </>
 
   )
 
