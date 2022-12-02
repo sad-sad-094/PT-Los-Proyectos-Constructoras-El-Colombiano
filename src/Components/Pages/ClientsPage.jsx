a6643f9394ff316f32a48f84d83b48906d8f3241
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-12-02 14:52:16 -0500

    DeleteClietnesProyectos

--- src/Components/Pages/ClientsPage.jsx
+++ src/Components/Pages/ClientsPage.jsx
@@ -1,19 +1,38 @@
 /* Author: Sebastian Aguirre Duque
 E-mail: sadw621@gmail.com */
 
+import { arrayRemove, doc, updateDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { MdDelete, MdUpdate } from 'react-icons/md';
 import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 
 import MainNavbar from '../Modules/MainNavbar';
+import { db } from '../../Utils/Firebase';
 
 function ClientsPage() {
 
-  let clientsA = useSelector((state) => state.userInfo.clients);
+  const navigation = useNavigate();
 
-  const [listClients] = useState(clientsA)
+  let clientsA = useSelector((state) => state.userInfo.clients);
+  let userName = useSelector((state) => state.userInfo.name);
+
+  const [listClients] = useState(clientsA);
+
+  const deleteProjects = (index) => {
+    updateDoc(doc(db, userName, `${userName}ID`), {
+      clients: arrayRemove(
+        listClients[index]
+      )
+    })
+      .then(() => {
+        toast.success('Cliente borrado satisfactoriamente.');
+        navigation("/home");
+      })
+  }
 
   return (
 
@@ -50,8 +69,8 @@ function ClientsPage() {
                       <td>{i.DNI}</td>
                       <td>{i.birth}</td>
                       <td>{i.project}</td>
-                      <td><MdDelete /></td>
-                      <td><MdUpdate /></td>
+                      <td><MdDelete onClick={() => deleteProjects(index)} style={{cursor: 'pointer'}} /></td>
+                      <td><MdUpdate style={{cursor: 'pointer'}} /></td>
                     </tr>
                   )
                 })
--- src/Components/Pages/ProjectsPage.jsx
+++ src/Components/Pages/ProjectsPage.jsx
@@ -1,19 +1,38 @@
 /* Author: Sebastian Aguirre Duque
 E-mail: sadw621@gmail.com */
 
+import { arrayRemove, doc, updateDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { MdDelete, MdUpdate } from 'react-icons/md';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 import MainNavbar from '../Modules/MainNavbar';
+import { db } from '../../Utils/Firebase';
+import { toast } from 'react-toastify';
 
 function ProjectsPage() {
 
+  const navigation = useNavigate();
+
   let projectsA = useSelector((state) => state.userInfo.projects);
-  
-  const [listProjects] = useState(projectsA)
+  let userName = useSelector((state) => state.userInfo.name);
+
+  const [listProjects] = useState(projectsA);
+
+  const deleteProjects = (index) => {
+    updateDoc(doc(db, userName, `${userName}ID`), {
+      projects: arrayRemove(
+        listProjects[index]
+      )
+    })
+      .then(() => {
+        toast.success('Proyecto borrado satisfactoriamente.');
+        navigation("/home");
+      })
+  }
 
   return (
 
@@ -50,8 +69,8 @@ function ProjectsPage() {
                       <td>{i.adress}</td>
                       <td>{i.builders}</td>
                       <td>{i.contact}</td>
-                      <td><MdDelete /></td>
-                      <td><MdUpdate /></td>
+                      <td><MdDelete onClick={() => deleteProjects(index)} style={{cursor: 'pointer'}} /></td>
+                      <td><MdUpdate style={{cursor: 'pointer'}} /></td>
                     </tr>
                   )
                 })
