83444c218e9ce1601b5b68a10dab29c125b31c6b
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-12-02 15:18:18 -0500

    ProtecciónRutas

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -2,23 +2,28 @@
 E-mail: sadw621@gmail.com */
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 import LandingPage from "../Components/Pages/LandingPage";
 import HomePage from "../Components/Pages/HomePage";
 import ProjectsPage from "../Components/Pages/ProjectsPage";
 import ClientsPage from "../Components/Pages/ClientsPage";
+import { PrivateRoutes, PublicRoutes } from "../Components/Modules/IsLogged";
 
 
 function AppRoutes() {
 
+  const logged = useSelector((state) => state.isLogged)
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/home" element={<HomePage/>} />
-        <Route path="/projects" element={<ProjectsPage/>} />
-        <Route path="/clients" element={<ClientsPage/>} />
+        <Route path="/" element={<PublicRoutes auth={logged}> <LandingPage/> </PublicRoutes>} />
+        
+        <Route path="/home" element={<PrivateRoutes auth={logged}> <HomePage/> </PrivateRoutes>} />
+        <Route path="/projects" element={<PrivateRoutes auth={logged}> <ProjectsPage/> </PrivateRoutes>} />
+        <Route path="/clients" element={<PrivateRoutes auth={logged}> <ClientsPage/> </PrivateRoutes>} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
